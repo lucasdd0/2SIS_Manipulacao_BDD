d7c107848dc5bb5589b177ed378af6f5838bf15c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-18 20:02:23 -0300

    commit

--- src/main/java/br/fiap/conexao/Main.java
+++ src/main/java/br/fiap/conexao/Main.java
@@ -3,10 +3,16 @@ package br.fiap.conexao;
 import br.fiap.modelo.Categoria;
 import br.fiap.modelo.CategoriaDAO;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         CategoriaDAO dao = new CategoriaDAO();
-        Categoria categoria = new Categoria(7L, "Festa");
-        dao.inserir(categoria);
+//        Categoria categoria = new Categoria(7L, "Festa");
+//        dao.inserir(categoria);
+        List<Categoria> lista = dao.listar();
+        for(Categoria categoria : lista){
+            System.out.println(categoria.getId() + " --> " + categoria.getCategoria());
+        }
     }
 }
--- src/main/java/br/fiap/modelo/CategoriaDAO.java
+++ src/main/java/br/fiap/modelo/CategoriaDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriaDAO {
     private PreparedStatement ps;
@@ -23,5 +25,24 @@ public class CategoriaDAO {
         catch (SQLException e){
             System.out.println("Erro  ao inserir no banco\n" + e);
         }
+    }
+
+    public List<Categoria> listar(){
+        List<Categoria> lista = new ArrayList<>();
+        sql = "SELECT * FROM java_categoria";
+
+        try(Connection connection = Conexao.conectar()) {
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                lista.add(new Categoria(rs.getLong("id_categoria"), rs.getString("categoria")));
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Erro  ao inserir no banco\n" + e);
+        }
+
+        return lista;
+
     }
 }
