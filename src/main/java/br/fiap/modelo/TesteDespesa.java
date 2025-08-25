edd0ae5d44a23ebafbf00a43ff3b42377a96e939
lucasdd0 <lucasfo2005@gmail.com>
2025-08-25 20:47:41 -0300

    commit

--- src/main/java/br/fiap/modelo/TesteDespesa.java
+++ src/main/java/br/fiap/modelo/TesteDespesa.java
@@ -1,12 +1,21 @@
 package br.fiap.modelo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class TesteDespesa {
     public static void main(String[] args) {
         DespesaDAO dao = new DespesaDAO();
 
         Despesa despesa = new Despesa();
+        DateTimeFormatter mascara = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-
+        despesa.setId(100L);
+        despesa.setDescricao("Viagem de helicóptero para Paraty");
+        despesa.setValor(65000.00);
+        despesa.setCategoria(new Categoria(1L, ""));
+        despesa.setDate(LocalDate.parse("25/08/2025", mascara));
+        dao.inserir(despesa);
 
 
     }
